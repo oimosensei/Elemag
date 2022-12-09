7a19bb850a3c0c3835b199c37c310d1cebf6afb7
DESKTOP-8V39121\中谷隼也 <mistymikan@gmail.com>
2022-12-09 18:34:00 +0900

    Add Some Packages

---
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -0,0 +1,14 @@
+﻿namespace KanKikuchi.AudioManager{
+
+/// <summary>
+/// BGMファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class BGMPath{
+
+	public const string BATTLE27  = "BGM/Battle/battle27";
+	public const string STAGE_BGM = "BGM/StageBGM";
+	public const string TITLE_BGM = "BGM/TitleBGM";
+
+}
+
+}
---
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -0,0 +1,21 @@
+﻿namespace KanKikuchi.AudioManager{
+
+/// <summary>
+/// SEファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class SEPath{
+
+	public const string CLEAR           = "SE/clear";
+	public const string DASH_SOUND      = "SE/ダッシュ/dashSound";
+	public const string JINGLE10        = "SE/Jingle/jingle10";
+	public const string MITSUDESU       = "SE/mitsudesu";
+	public const string ROPE_SOUND      = "SE/ropeSound";
+	public const string ROPE_SOUND2     = "SE/ropeSound2";
+	public const string SHOT_SOUND      = "SE/発射音/shotSound";
+	public const string SOCIAL_DISTANCE = "SE/socialDistance";
+	public const string SPRING_SOUND    = "SE/springSound";
+	public const string YARARE          = "SE/yarare";
+
+}
+
+}
