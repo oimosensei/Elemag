4f6d8c329e1506714313e7ac137a677882c1e534
DESKTOP-8V39121\中谷隼也 <mistymikan@gmail.com>
2022-12-30 16:07:52 +0900

    Probuilderの追加等

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Observable.IntervalFrame(10)
+            .Subscribe(_ =>
+            {
+                Debug.Log("test");
+            });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
