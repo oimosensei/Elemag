faca084c378602a445e294606cb8ff86186f2686
DESKTOP-8V39121\中谷隼也 <mistymikan@gmail.com>
2023-01-23 13:37:06 +0900

    SkyBoxアセットの追加

---
+++ Assets/Scripts/CameraEnable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace raspberly.ovr
+{
+    public class CameraEnable : MonoBehaviour
+    {
+        void Start()
+        {
+            GetComponent<Camera>().enabled = false;
+            GetComponent<Camera>().enabled = true;
+        }
+    }
+}
